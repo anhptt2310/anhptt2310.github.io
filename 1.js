312390e356fba257e3dd10e6d25526414f2ef0d7
unknown <theanhdx1@gmail.com>
2018-04-03 21:34:48 +0700

    att

--- 1.js
+++ 1.js
@@ -1,9 +1,17 @@
 $(function() {
 	var posEdu = $('#edu-cube').offset().top;
-	console.log(posEdu);
 	$('.mouse').click(function(event) {
 		/* Act on the event */
 		console.log("day ne");
 		$('html,body').animate({scrollTop:$('#edu-cube').offset().top},700);
 	});
+	$('.abt-me').click(function(event) {
+		/* Act on the event */
+		$('html,body').animate({scrollTop:$('#edu-cube').offset().top},700);
+	});
+	$('.mybl').click(function(event){
+		event.preventDefault();
+		alert('Sorry , I havent had blog :(');
+	});
+
 });
